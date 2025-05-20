07a9e0477d8a7f75a9a42e2bc75d1b1de81f5f55
Pedre Gabriel <gabriel.pedre974@gmail.com>
2025-05-20 14:02:16 +0400

    Initial commit

---
+++ src/main/java/com/gaby/miniprojetblogrecettes/MiniProjetBlogRecettesApplication.java
@@ -0,0 +1,13 @@
+package com.gaby.miniprojetblogrecettes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetBlogRecettesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetBlogRecettesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaby/miniprojetblogrecettes/MiniProjetBlogRecettesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaby.miniprojetblogrecettes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetBlogRecettesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
